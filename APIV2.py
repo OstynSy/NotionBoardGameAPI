483d4740773fc0cf437939d7b3147754433b9ff8
Ostyn Sy <osy304@insite.4cd.edu>
2023-03-19 23:38:25 -0700

    Updates the database. Mostly completed. Needs Cleanup.

--- APIV2.py
+++ APIV2.py
@@ -37,18 +37,22 @@ for page in results["results"]:
     if (page["properties"]["Name"]["title"] != []) and (page["properties"]["Integration Status"]["rich_text"] == []):
         list_empty_pages.append(page)
 
-pprint(len(list_empty_pages))
+#pprint(len(list_empty_pages))
 # Set up Boardgamegeek API parameters
 base_url = "https://www.boardgamegeek.com/xmlapi2/"
 search_path = "search?type=boardgame&query="
 thing_path = "thing?type=boardgame&stats=1&id="
-
+error = ""
 # Search for BGG Data on each page
 for page in list_empty_pages:
     #Search for board game information using the Boardgamegeek API
+    pprint(page)
     bg_name = page["properties"]["Name"]["title"][0]["plain_text"]
+    page_id = page["id"]
+
     pprint("")
     pprint(bg_name)
+    pprint(page_id)
 
     response = requests.get(base_url + search_path + bg_name)
     xml_data = response.content
@@ -56,25 +60,128 @@ for page in list_empty_pages:
     total = int(json_data.get("items", {}).get("@total", 0))
     items = json_data.get("items", {}).get("item", [])
 
-
     # Only Fuzzy match if there are more than 1 item (4 indicates more than 4 fields in the json)
-    pprint(total)
-
+    pprint(f"Search Results: {total}")
+    best_match_id = -1
     # BGG found 0 results
-    if total == 0:
-        #report error that BGG can not find that item
-        pprint("no results")
-    # BGG found multiple results
-    elif total > 1:
-        best_match_id = fuzzyMatch(bg_name, items)
-        pprint(f"Best match ID: {best_match_id}")
-    else:
-        pprint(items)
-        best_match_id = items["@id"]
-        pprint(f"Best match ID: {best_match_id}")
+    try:
+        if total == 0:
+            #report error that BGG can not find that item
+            error = "No Results Found on BGG. Try being more accurate with your name. It is also possible that BGG does not have data on your game"
+            pprint("no results")
+            # BGG found multiple results
+        elif total > 1:
+            best_match_id = fuzzyMatch(bg_name, items)
+            pprint(f"Best match ID: {best_match_id}")
+        else:
+            best_match_id = items["@id"]
+            pprint(f"Best match ID: {best_match_id}")
+
+        if best_match_id != -1:
+            game_info_response = requests.get(base_url + thing_path + str(best_match_id))
+            game_info_data = game_info_response.content
+    except:
+        error= "There were issues searching for results!"
+        pprint("There was an error with the results")
+
+    try:
+        game_info = game_info = xmltodict.parse(game_info_data)['items']['item']
+
+        #Store Json values as Variables
+        image_url = {
+            "name": "Image.png",
+            "external":{
+                "url": game_info["image"]
+            } 
+        }
+
+        min_players = game_info["minplayers"]["@value"]
+        max_players = game_info["maxplayers"]["@value"]
+        min_time = game_info["minplaytime"]["@value"]
+        max_time = game_info["maxplaytime"]["@value"]
+
+        genre = {
+            "name": game_info["link"][0]["@value"]
+        }
+
+        ratings = game_info["statistics"]["ratings"]
+
+        geek_rating = game_info["statistics"]["ratings"]["bayesaverage"]["@value"]
+        avg_rating = game_info["statistics"]["ratings"]["average"]["@value"]
+        num_votes = game_info["statistics"]["ratings"]["usersrated"]["@value"]
+
+        integration_status = "OK"
+        error = ""
+        pprint(integration_status)
+        #pprint(game_info)
+
+    except:
+        integration_status = "BAD"
+        error = "Trouble Populating Data after finding the ID of board game OR BGG does not provide the data"
+        pprint(integration_status)
+
+    notion.pages.update(page_id= page_id, properties={
+        "Image": {"files": [image_url]},
+        "Min. Players": {"number": int(min_players)},
+        "Max. Players": {"number": int(max_players)},
+        "Min. Time": {"number": int(min_time)},
+        "Max. Time": {"number": int(max_time)},
+        "Genre": {"multi_select": [genre]},
+        "Geek Rating": {"number": round(float(geek_rating),2)},
+        "Avg. Rating": {"number": round(float(avg_rating),2)},
+        "Num Votes": {"number": int(num_votes)},
+        "Integration Status": {"rich_text": [{"text": {"content": integration_status}}]},
+        "Error": {"rich_text": [{"text": {"content": error}}]},
+    })
+
+    #pprint(list_empty_pages)
+
+    
+# create a list of updates
+# updates = []
+# for page in list_empty_pages:
+#     update = {
+#         "id": page["id"],
+#         "properties": {
+#             "Image": {"files": [image_url]},
+#             "Min. Players": {"number": int(min_players)},
+#             "Max. Players": {"number": int(max_players)},
+#             "Min. Time": {"number": int(min_time)},
+#             "Max. Time": {"number": int(max_time)},
+#             "Genre": {"multi_select": [genre]},
+#             "Geek Rating": {"number": round(float(geek_rating),2)},
+#             "Avg. Rating": {"number": round(float(avg_rating),2)},
+#             "Num Votes": {"number": int(num_votes)},
+#             "Integration Status": {"rich_text": [{"text": {"content": integration_status}}]},
+#             "Error": {"rich_text": [{"text": {"content": error}}]},
+#         }
+#     }
+#     updates.append(update)
+
+# # update the database
+# notion.databases.batch_update(
+#     database_id=bg_database_id,
+#     updates=updates
+# )
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#depricated
 # # working retrieves the title of last element in db
 # results = notion.databases.query(
 #     database_id=board_game_id,
@@ -98,49 +205,3 @@ for page in list_empty_pages:
 
 
 
-
-# game_info_response = requests.get(base_url + thing_path + str(best_match_id))
-# game_info_data = game_info_response.content
-# game_info = game_info = xmltodict.parse(game_info_data)['items']['item']
-# #pprint(game_info)
-
-# # Store Json values as Variables
-# image_url = {
-#     "name": "Image.png",
-#     "external":{
-#         "url": game_info["image"]
-#     } 
-# }
-
-# min_players = game_info["minplayers"]["@value"]
-# max_players = game_info["maxplayers"]["@value"]
-# min_time = game_info["minplaytime"]["@value"]
-# max_time = game_info["maxplaytime"]["@value"]
-
-# genre = {
-#     "name": game_info["link"][0]["@value"]
-# }
-
-# ratings = game_info["statistics"]["ratings"]
-
-# geek_rating = game_info["statistics"]["ratings"]["bayesaverage"]["@value"]
-# avg_rating = game_info["statistics"]["ratings"]["average"]["@value"]
-# num_votes = game_info["statistics"]["ratings"]["usersrated"]["@value"]
-
-# # integration_status = "ok"
-# # error = Null
-
-# #Update the Notion database with board game information
-# notion.pages.update(page_id= last_page_id, properties={
-#     "Image": {"files": [image_url]},
-#     "Min. Players": {"number": int(min_players)},
-#     "Max. Players": {"number": int(max_players)},
-#     "Min. Time": {"number": int(min_time)},
-#     "Max. Time": {"number": int(max_time)},
-#     "Genre": {"multi_select": [genre]},
-#     "Geek Rating": {"number": round(float(geek_rating),2)},
-#     "Avg. Rating": {"number": round(float(avg_rating),2)},
-#     "Num Votes": {"number": int(num_votes)},
-#     #"Integration Status": integration_status,
-#     #"Error": error,
-# })
